bded9f187f08668e30fdd38a8f24d526884ec482
calvindiaz <ga2023135619@gmail.com>
2023-07-18 20:37:14 +0800

    重複註冊 刪除

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import { register } from "swiper/element/bundle";
 
 import "@/assets/scss/style.scss";
 
-// swiper
-import { register } from "swiper/element/bundle";
-
 const app = createApp(App);
 app.use(store);
 app.use(router);
