481b24f5159f370102886d4d1af51acf52023dff
calvindiaz <ga2023135619@gmail.com>
2023-08-09 13:34:09 +0800

    No commit

--- public/www/phps/postSpot.php
+++ public/www/phps/postSpot.php
@@ -16,20 +16,14 @@ if($_FILES["image"]["error"] === 0) {
       require_once("./connect_chd102g1.php");
       $sql = "insert into spot (`spot_no`, `spot_name`, `spot_info`, `spot_status`, `spot_file`)
         values (null, :spot_name, :spot_info, :spot_status, :spot_file);";
-      $pattern = $pdo->prepare($sql); 
+      $spot = $pdo->prepare($sql);
 
-      // pattern_no 這裡是AUTO_INCREMENT，不需要設定
-      $pattern->bindValue(":spot_name", $_POST["spot_name"]);
-      $pattern->bindValue(":spot_info", $_POST["spot_info"]);
-      $pattern->bindValue(":spot_file", "images/pattern/$fileName");//添加圖檔路徑
-
-
-// 添加以下代碼進行調試
-// echo "pattern_name: ".$_POST["pattern_name"]."<br>";
-// echo "pattern_desc: ".$_POST["pattern_desc"]."<br>";
-
-
-      $pattern->execute();
+      // spot_no 這裡是AUTO_INCREMENT，不需要設定
+      $spot->bindValue(":spot_name", $_POST["spot_name"]);
+      $spot->bindValue(":spot_info", $_POST["spot_info"]);
+      $spot->bindValue(":spot_status", $_POST["spot_status"]);
+      $spot->bindValue(":spot_file", "images/pattern/$fileName");//添加圖檔路徑
+      $spot->execute();
       echo "新增成功~<br>";
     } catch (PDOException $e) {
       echo "錯誤行號 : ", $e->getLine(), "<br>";
