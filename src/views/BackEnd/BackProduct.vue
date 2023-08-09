35b100bb1705868c223f06d5fd5e8810b6b332f2
tsuki-guardin <tsuki852456@gmail.com>
2023-08-09 13:34:28 +0800

    後台商品頁製作中

--- src/views/BackEnd/BackProduct.vue
+++ src/views/BackEnd/BackProduct.vue
@@ -36,11 +36,14 @@
         <th scope="col"></th>
       </tr>
     </thead>
-    <!-- <tbody>
-      <tr v-for="(item, index) in filteredItems" :key="index">
-        <th scope="row">{{ item.id }}</th>
-        <td class="ellipsis">{{ item.name }}</td>
-        <td class="ellipsis">{{ item.qty }}</td>
+    <tbody>
+      <tr v-for="(item, index) in productData" :key="index">
+        <th scope="row">{{ item.prod_no }}</th>
+        <td class="ellipsis">{{ item.prod_name }}</td>
+        <td class="ellipsis">{{ item.prod_summary }}</td>
+        <td class="ellipsis">{{ item.prod_price }}</td>
+        <td class="ellipsis">{{ item.prod_status }}</td>
+        <td class="ellipsis">{{ item.prod_file }}</td>
         <td style="text-align: right">
           <button
             type="button"
@@ -52,15 +55,164 @@
           </button>
         </td>
       </tr>
-    </tbody> -->
+    </tbody>
 
     <p v-if="filteredItems.length === 0" class="text-danger">
       * 沒有找到符合搜尋條件的結果
     </p>
   </table>
 
+<!-- edit modal -->
+  <form
+    action="http://localhost:80/phps/editProduct.php"
+    method="post"
+    enctype="multipart/form-data"
+    v-if="showModal"
+    class="modal fade"
+    id="itemModal"
+    tabindex="-1"
+    aria-labelledby="itemModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" style="max-width: 80%">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="itemModalLabel">修改客製車票圖案</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            @click="cancelChanges"
+          ></button>
+        </div>
+
+        <!-- modal body -->
+        <div
+          style="display: flex; flex-direction: column"
+          class="modal-body gap-2"
+        >
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >商品編號</span
+            >
+            <input
+              disabled
+              v-model="currentItem.pattern_no"
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+          </div>
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >圖案名稱</span
+            >
+            <input
+              v-model="currentItem.pattern_name"
+              name="pattern_name"
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+          </div>
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >圖案描述</span
+            >
+            <input
+              v-model="currentItem.pattern_desc"
+              name="pattern_desc"
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+          </div>
+
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >創建日期</span
+            >
+            <input
+              disabled
+              v-model="currentItem.creation_date"
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+          </div>
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >圖案檔案</span
+            >
+            <input
+              disabled
+              v-model="currentItem.pattern_file"
+              name="pattern_file"
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"
+            />
+          </div>
+          <div class="input-group input-group-lg">
+            <span class="input-group-text" id="inputGroup-sizing-lg"
+              >上傳檔案</span
+            >
+            <input
+              type="file"
+              class="form-control"
+              id="inputGroupFile02"
+              @change="handleFileUpload"
+            />
+          </div>
+          <div class="model_body_pic">
+            <Images
+              v-if="currentItem.pattern_file"
+              :imgURL="`${currentItem.pattern_file}`"
+              :alt="`Image preview`"
+            />
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-danger"
+            data-bs-dismiss="modal"
+            @click="deleteAnnouncement"
+          >
+            刪除圖案
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-dismiss="modal"
+            @click="saveChanges"
+          >
+            儲存變更
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
+
   <!-- new modal -->
   <div
+    class="modal fade"
+    id="itemNewModal"
+    tabindex="-1"
+    aria-labelledby="itemModalLabel"
+    aria-hidden="true"
+  >
+  <form
+    action="http://localhost:80/phps/postProduct.php"
+    method="post"
+    enctype="multipart/form-data"
     class="modal fade"
     id="itemNewModal"
     tabindex="-1"
@@ -84,7 +236,7 @@
                   >商品編號</span
                 >
                 <input
-                  v-model="newAnnouncement.id"
+                  v-model="newProduct.prod_no"
                   type="text"
                   class="form-control"
                   aria-label="Sizing example input"
@@ -97,7 +249,7 @@
                   >商品名稱</span
                 >
                 <input
-                  v-model="newAnnouncement.name"
+                  v-model="newProduct.prod_name"
                   type="text"
                   class="form-control"
                   aria-label="Sizing example input"
@@ -110,7 +262,7 @@
                   >商品介紹</span
                 >
                 <input
-                  v-model="newAnnouncement.summary"
+                  v-model="newProduct.prod_summary"
                   type="text"
                   class="form-control"
                   aria-label="Sizing example input"
@@ -123,7 +275,7 @@
                   >商品價格</span
                 >
                 <input
-                  v-model="newAnnouncement.price"
+                  v-model="newProduct.prod_price"
                   type="text"
                   class="form-control"
                   aria-label="Sizing example input"
@@ -136,7 +288,7 @@
                   >商品狀態</span
                 >
                 <input
-                  v-model="newAnnouncement.status"
+                  v-model="newProduct.prod_status"
                   type="text"
                   class="form-control"
                   aria-label="Sizing example input"
@@ -165,26 +317,31 @@
             <button
               class="btn btn-secondary"
               data-bs-dismiss="modal"
-              @click="clearAnnouncement()"
+              @click="clearProduct()"
             >
               取消
             </button>
-            <button class="btn btn-primary" @click="submitAnnouncement()">
+            <button class="btn btn-primary" @click="submitProduct()">
               確定新增
             </button>
           </slot>
         </div>
       </div>
     </div>
+    </form>
   </div>
+  <div>{{ productData }}</div>
 </template>
 
 <script>
+import axios from "axios";
 import { Modal } from "bootstrap";
+import { BASE_URL } from "@/assets/js/common.js";
 
 export default {
   data() {
     return {
+      productData: [],
       items: [
         {
           // id: 1,
@@ -200,13 +357,13 @@ export default {
       showModal: false,
       selectedFile: null,
       // new model
-      newAnnouncement: {
-        id: "", // 確保 id 屬性存在
-        name: "",
-        summary: "",
-        price: "",
-        status: "",
-        file: "",
+      newProduct: {
+        prod_no: "", // 確保 id 屬性存在
+        prod_name: "",
+        prod_summary: "",
+        prod_price: "",
+        prod_status: "",
+        prod_file: "",
       },
     };
   },
@@ -226,6 +383,8 @@ export default {
 
   methods: {
     // model
+
+    //儲存變更
     saveChanges() {
       // 在這裡更新資料
       // 如有需要，你也可以將 currentItem 傳到後端
@@ -268,42 +427,34 @@ export default {
     },
 
     // new model
-    submitAnnouncement() {
+    submitProduct() {
       if (
-        !this.newAnnouncement.id ||
-        !this.newAnnouncement.name ||
-        !this.newAnnouncement.summary ||
-        !this.newAnnouncement.price ||
-        !this.newAnnouncement.status ||
-        !this.newAnnouncement.file
+        !this.newProduct.prod_no ||
+        !this.newProduct.prod_name ||
+        !this.newProduct.prod_summary ||
+        !this.newProduct.prod_price ||
+        !this.newProduct.prod_status ||
+        !this.newProduct.prod_file
       ) {
         alert("所有欄位都必須填寫！");
         return;
       }
 
-      console.log(this.newAnnouncement);
-      this.clearAnnouncement();
-      this.newAnnouncement = {
-        id: "",
-        name: "",
-        summary: "",
-        price: "",
-        status: "",
-        file: "",
-      };
+      console.log(this.newProduct);
+      this.clearProduct();
       const modalEl = document.getElementById("itemNewModal");
       const modalInstance = Modal.getInstance(modalEl);
       modalInstance.hide();
     },
 
-    clearAnnouncement() {
-      this.newAnnouncement = {
-        id: "",
-        name: "",
-        summary: "",
-        price: "",
-        status: "",
-        file: "",
+    clearProduct() {
+      this.newProduct = {
+        prod_no: "",
+        prod_name: "",
+        prod_summary: "",
+        prod_price: "",
+        prod_status: "",
+        prod_file: "",
       };
     },
 
@@ -317,6 +468,16 @@ export default {
         this.showModal = false;
       }
     },
+  },
+  mounted() {
+    axios
+      .get(`${BASE_URL}/getProduct.php`)
+      .then((response) => {
+        this.productData = response.data;
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data:", error);
+      });
   },
 };
 </script>
