72291318cf878e821bb04f2ee6c01dfc1d6381d0
sunny96087 <sunny96087@yahoo.com.tw>
2023-08-11 13:09:36 +0800

    forum php

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -2,5 +2,5 @@
 // export const BASE_URL = "https://tibamef2e.com/chd102/g1/phps";
 // 測 Localhost 用
 export const BASE_URL = "http://localhost/phps/";
-// Mac可能要自已打開唷><
-// export const BASE_URL = "http://localhost:8888/cgd103_g1/public/api";
+// Mac 用
+export const MAC_URL = "http://localhost:8080/phps/";
