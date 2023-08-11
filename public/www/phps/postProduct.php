ca0a992b45f86d9420f4ae0633d22b926125e416
tsuki-guardin <tsuki852456@gmail.com>
2023-08-12 00:44:18 +0800

    要拉資料 先推一版

--- public/www/phps/postProduct.php
+++ public/www/phps/postProduct.php
@@ -1,39 +1,53 @@
-<?php 
-if($_FILES["image"]["error"] === 0) {
-  $dir = "../../images/pattern/"; //指定所要上傳的路徑
+<?php
+if($_FILES["new_img"]["error"] === 0) {
+  $dir = "../../images/online-mall/"; //指定所要上傳的路徑
   if(file_exists($dir)===false){
       mkdir($dir); //make directory
   }
 
-  $from = $_FILES["image"]["tmp_name"];
+  $newProduct = json_decode(file_get_contents("php://input"), true);
 
-  $to = $dir . $_FILES["image"]["name"];
+  $from = $_FILES["new_img"]["tmp_name"];
 
-  $fileName = $_FILES["image"]["name"]; // 將檔案名稱設定給 $fileName
+  //處理檔名 -> 建立唯一性--------------------
+  //---產生主檔名
+  $fileName = uniqid(); //13個隨機字符
+  //---取出副檔名   
+  $fileExt = pathInfo($_FILES["new_img"]["name"],
+  PATHINFO_EXTENSION); //取得物件格式裡的檔案名稱後再處理: 01.svg -> .svg
+
+  $fileName = "$fileName.$fileExt";//用uniqid()去串接副檔名
+
+  $to = $dir . $fileName;
+
   if(copy($from, $to)){
     //寫入資料庫
     try {
       require_once("./connect_chd102g1.php");
-      $sql = "insert into pattern (`pattern_no`, `pattern_name`, `pattern_file`, `pattern_desc`, `creation_date`)
-        values (null, :pattern_name, :pattern_file, :pattern_desc, :creation_date);";
-      $pattern = $pdo->prepare($sql); 
-      $creation_date = date("Y-m-d");// 自動生成當下時間
+      $sql = "INSERT INTO product (`prod_name`, `prod_summary`, `prod_price`, `prod_status`, `prod_file`, `prod_type`, `prod_hot`)
+              VALUES (:prod_name, :prod_summary, :prod_price, :prod_status, :prod_file, :prod_type, :prod_hot";
+      $product = $pdo->prepare($sql); 
 
-      // pattern_no 這裡是AUTO_INCREMENT，不需要設定
-      $pattern->bindValue(":pattern_name", $_POST["pattern_name"]);
-      $pattern->bindValue(":pattern_desc", $_POST["pattern_desc"]);
-      $pattern->bindValue(":creation_date", $creation_date);
-      $pattern->bindValue(":pattern_file", "images/pattern/$fileName");//添加圖檔路徑
-      $pattern->execute();
+      // product_no 這裡是AUTO_INCREMENT，不需要設定
+      $product->bindParam(":prod_name", $newProduct["prod_name"]);
+      $product->bindParam(":prod_summary", $newProduct["prod_summary"]);
+      $product->bindParam(":prod_price", $newProduct["prod_price"]);
+      $product->bindParam(":prod_status", $newProduct["prod_status"]);
+      // creation_date SQL 指令生成當下日期
+      $product->bindValue(":prod_file", "images/online-mall/$fileName");//添加圖檔路徑
+      $product->bindParam(":prod_type", $newProduct["prod_type"]);
+      $product->bindParam(":prod_hot", $newProduct["prod_hot"]);
+      $product->execute();
+
       echo "新增成功~<br>";
     } catch (PDOException $e) {
       echo "錯誤行號 : ", $e->getLine(), "<br>";
       echo "錯誤原因 : ", $e->getMessage(), "<br>";
-      //echo "系統暫時不能正常運行，請稍後再試<br>";  
+      echo "系統暫時不能正常運行，請稍後再試<br>";  
     }
 
   }else{
-      echo "寫入失敗";
+    echo "寫入失敗";
   }
 
 } else {
---
+++ public/www/phps/test.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type:application/json;charset=utf-8");
+
+  $newProduct = json_decode(file_get_contents("php://input"), true);
+
+    //寫入資料庫
+    try {
+      require_once("./connect_chd102g1.php");
+      $sql = "INSERT INTO product (`prod_name`, `prod_summary`, `prod_price`, `prod_status`, `prod_file`, `prod_type`, `prod_hot`)
+              VALUES (:prod_name, :prod_summary, :prod_price, :prod_status, :prod_file, :prod_type, :prod_hot)";
+      $product = $pdo->prepare($sql); 
+
+      // product_no 這裡是AUTO_INCREMENT，不需要設定
+      $product->bindParam(':prod_name', $newProduct["prod_name"]);
+      $product->bindParam(':prod_summary', $newProduct["prod_summary"]);
+      $product->bindParam(':prod_price', $newProduct["prod_price"]);
+      $product->bindParam(':prod_status', $newProduct["prod_status"]);
+      $product->bindParam(':prod_file', $newProduct["prod_file"]);
+      $product->bindParam(':prod_type', $newProduct["prod_type"]);
+      $product->bindParam(':prod_hot', $newProduct["prod_hot"]);
+      $product->execute();
+
+      echo "新增成功~<br>";
+    } catch (PDOException $e) {
+      echo "錯誤行號 : ", $e->getLine(), "<br>";
+      echo "錯誤原因 : ", $e->getMessage(), "<br>";
+      echo "系統暫時不能正常運行，請稍後再試<br>";  
+    }
+
+
+
+?>
