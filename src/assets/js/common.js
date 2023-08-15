37e1258964c648dbf0d2f846f688e1786dfc6aec
vicky5645 <vicky564564@gmail.com>
2023-08-15 20:40:28 +0800

    測試部署到tibame上的設定修改

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,6 +1,7 @@
 // 測 TibaMe 用
-// export const BASE_URL = "https://tibamef2e.com/chd102/g1/phps";
+export const BASE_URL = "https://tibamef2e.com/chd102/g1/phps/";
 // 測 Localhost 用
-export const BASE_URL = "http://localhost/phps/";
+// export const BASE_URL = "http://localhost/phps/";
 // Mac 用
-export const MAC_URL = "http://localhost:8080/phps/";
+// export const MAC_URL = "http://localhost:8080/phps/";
+export const MAC_URL = "https://tibamef2e.com/chd102/g1/phps/";
